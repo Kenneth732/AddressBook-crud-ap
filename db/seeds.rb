07de19cde2b12c7de703e8ca6ba1486af0f5ec50
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-12 12:56:21 +0300

    Task

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create!(
+
+)
+
+Task.create!(
+
+)
 
 # Add more contacts as needed
