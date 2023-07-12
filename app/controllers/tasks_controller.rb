22ce5bc0a01babb597ab52cea3b479aed5261625
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-12 12:59:03 +0300

    show function for displaying contact by its id number

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,10 @@ class TasksController < ApplicationController
       render json: tasks 
     end
   
-
+    def show
+      task = Task.find_by(id: params[:id])
+      render json: task
+    end
+  
   end
   
