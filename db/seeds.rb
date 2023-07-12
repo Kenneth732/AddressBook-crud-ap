860f240d33edac83dbb1398f0cc172f42ef6e976
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-12 12:56:45 +0300

    data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,11 @@ Task.create!(
 )
 
 Task.create!(
-
+  firstname: "Jane",
+  lastname: "Doe",
+  phonenumber: "987-654-3210",
+  emailaddress: "janedoe@example.com",
+  image: "https://cdn.leonardo.ai/users/8443c141-23d4-437d-a957-a223d2e70b7f/generations/cf51a1cf-4f53-4058-8432-cceedccf8e6b/variations/Default_Intricate_portrait_of_a_very_old_African_woman_with_wr_1_cf51a1cf-4f53-4058-8432-cceedccf8e6b_1.jpg?w=512"
 )
 
 # Add more contacts as needed
