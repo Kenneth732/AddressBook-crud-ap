fd4e32d31893a0b9ac6e22562754f633ce6bb0cc
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-12 13:04:47 +0300

    validate image

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,6 @@ class Task < ApplicationRecord
     validates :lastname, presence: true, length: { maximum: 50 }
     validates :phonenumber, presence: true, format: { with: /\A[0-9+\(\)#\.\s\/ext-]+\z/, message: "Invalid phone number format" }, length: { minimum: 10, maximum: 15 }
     validates :emailaddress, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { maximum: 255 }
-
+    validates :image, format: { with: /\.(png|jpg|jpeg|gif)\z/i, message: "Invalid image format" }, allow_blank: true
 end
   
