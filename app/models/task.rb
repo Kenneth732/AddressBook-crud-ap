f98a09127ea83f8e88db32a870a8ffe9495cb5a4
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-12 13:04:24 +0300

    validate emailaddress

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
     validates :firstname, presence: true, length: { maximum: 50 }
     validates :lastname, presence: true, length: { maximum: 50 }
     validates :phonenumber, presence: true, format: { with: /\A[0-9+\(\)#\.\s\/ext-]+\z/, message: "Invalid phone number format" }, length: { minimum: 10, maximum: 15 }
+    validates :emailaddress, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { maximum: 255 }
 
 end
   
