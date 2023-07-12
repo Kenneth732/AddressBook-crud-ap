d7c0f5da4c82e3b70019e6e9ad7764ba056debb1
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-12 13:03:52 +0300

    validate phone number

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
     validates :firstname, presence: true, length: { maximum: 50 }
     validates :lastname, presence: true, length: { maximum: 50 }
+    validates :phonenumber, presence: true, format: { with: /\A[0-9+\(\)#\.\s\/ext-]+\z/, message: "Invalid phone number format" }, length: { minimum: 10, maximum: 15 }
 
 end
   
