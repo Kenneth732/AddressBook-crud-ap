220335aa9fca0e87171bc67b9e23072a66e61b58
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-12 12:59:37 +0300

    create function for creating new contact for user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,5 +9,15 @@ class TasksController < ApplicationController
       render json: task
     end
   
+    def create 
+      task = Task.new(task_params)
+      if task.save 
+        render json: task, status: :created
+      else
+        render json: task.errors, status: :unprocessable_entity
+      end
+    end
+  
+  
   end
   
